f9a74892eaf616340cdb426e1735295e1a28b28c
momu54 <96234201+momu54@users.noreply.github.com>
2024-04-21 12:09:31 +0800

    feat: Add dev build mode

--- scripts/build.ts
+++ scripts/build.ts
@@ -6,6 +6,11 @@ import 'dotenv/config';
 	const newContent = orignalContent
 		.replace('{{blockpage.head}}', await readFile('pages/head.html', 'utf-8'))
 		.replace('{{blockpage.body}}', await readFile('pages/body.html', 'utf-8'))
-		.replace('{{api_host}}', process.env.API_HOST || 'api.example.com');
+		.replace(
+			'{{api_host}}',
+			process.argv[2] === 'dev' || !process.env.API_HOST
+				? 'localhost:9443'
+				: process.env.API_HOST
+		);
 	await writeFile('dist/youtube.js', newContent);
 })();
